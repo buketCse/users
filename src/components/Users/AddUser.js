8e00ee931f6a082087e77f4106c851712b4814c8
buket.algan <buket.algan@solvoyo.com>
2022-08-18 09:14:21 +0300

    Add card and addUser components

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,22 @@
 import React from 'react';
-
+import Card from '../UI/Card';
+import classes from './AddUser.module.css'
+import buttonCls from './Button.module.css'
 const AddUser = props=>{
     const addUserHandler = (event) =>{
         event.preventDefault()
     }
 
     return (
-        <form onSubmit={addUserHandler}>
+        <Card className={classes.input}>
+        <form onSubmit={addUserHandler} >
             <label htmlFor="username">Username</label>
             <input id="username" type="text"></input>
             <label htmlFor="username">Age (Years)</label>
             <input id="username" type="number"></input>
-            <button type="submit">Add User</button>
+            <button type="submit" className={buttonCls.button}>Add User</button>
         </form>
+        </Card>
     )
 }
 
